ff6365b12d61e6c35aee655c8e42c8a538019537
tefots <tefotsiu0@gmail.com>
2024-11-13 11:52:54 +0200

    rmoved home link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
