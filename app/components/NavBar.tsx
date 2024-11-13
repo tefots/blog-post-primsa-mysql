23bbe737f0af366b4427468e74ca723074b51108
tefots <tefotsiu0@gmail.com>
2024-11-13 11:55:09 +0200

    u

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Menu, X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -65,12 +65,7 @@ export default function Navbar() {
 
             {/* Desktop menu */}
             <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              <Link
-                href="/"
-                className="border-primary text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-              >
-                Home
-              </Link>
+             
               <button
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 onClick={handleBlogs}
