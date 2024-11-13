a865fc12ebb85843684e16d1663715b74835d9b5
tefots <tefotsiu0@gmail.com>
2024-11-13 11:40:12 +0200

    update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -80,7 +80,7 @@ export default function Navbar() {
               {isLoggedIn ? (
                 <>
                   <Link
-                    href="/services"
+                    href="/pages/CreatePost"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     Write Blog
@@ -168,23 +168,23 @@ export default function Navbar() {
           >
             Home
           </Link>
-          <Link
-            href="/about"
+          <button
+            
             className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             onClick={handleBlogs}
           >
             Blogs
-          </Link>
+          </button>
           {isLoggedIn ? (
             <>
               <Link
-                href="/services"
+                href="/pages/CreatePost"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Write Blog
               </Link>
               <Link
-                href="/contact"
+                href="#"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Contact
@@ -193,7 +193,7 @@ export default function Navbar() {
           ) : (
             <>
               <Link
-                href="/signin"
+                href="/pages/Auth"
                 className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Sign In
@@ -220,6 +220,7 @@ export default function Navbar() {
                   width={40}
                   height={40}
                 />
+                
               </div>
               <div className="ml-3">
                 <div className="text-base font-medium text-gray-800">
